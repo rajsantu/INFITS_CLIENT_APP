439b90323f14c0bf708a112fb791bbab74be6404
Faizan2907 <zaan9823@gmail.com>
2023-04-03 20:15:29 +0530

    Updated code by Faizan (Live Screen)

--- app/src/main/java/com/example/infits/LiveScreen.java
+++ app/src/main/java/com/example/infits/LiveScreen.java
@@ -1,14 +1,227 @@
 package com.example.infits;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import io.agora.rtc2.*;
+import io.agora.rtc2.video.VideoCanvas;
+
 
 public class LiveScreen extends AppCompatActivity {
+
+    // chats, conversations, dititian and add_client
+
+    private RecyclerView recyclerView;
+    private EditText editText;
+    private ImageView sendButton, closeLiveScreen;
+    private List<Message> messages;
+    private MessageAdapter adapter;
+
+    // Fill the App ID of your project generated on Agora Console.
+    private final String appId = "d7147818a48f4f68a82894409665ffcb";
+    // Fill the channel name.
+    private String channelName = "analysed";
+    // Fill the temp token generated on Agora Console.
+    private String token = "007eJxTYPDTK7++2/Hi693tOVtEFcvcm08KFEo94GJekdM+UbCIdZkCQ4q5oYm5haFFoolFmkmamUWihZGFpYmJgaWZmWlaWnLSVxGNlIZARoaarBZmRgYIBPE5GBLzEnMqi1NTGBgAMNMemw==";
+    // An integer that identifies the local user.
+    private int uid = 0;
+    private boolean isJoined = false;
+
+    private RtcEngine agoraEngine;
+    //SurfaceView to render local video in a Container.
+    private SurfaceView localSurfaceView;
+    //SurfaceView to render Remote video in a Container.
+    private SurfaceView remoteSurfaceView;
+
+
+    private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS =
+            {
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.CAMERA
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_screen);
+
+        if (!checkSelfPermission()) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, PERMISSION_REQ_ID);
+        }
+        setupVideoSDKEngine();
+
+        recyclerView = findViewById(R.id.messageRecyclerView);
+        editText = findViewById(R.id.live_screen_message);
+        sendButton = findViewById(R.id.send_message_send_btn);
+        closeLiveScreen = findViewById(R.id.closeLiveScreen);
+
+        messages = new ArrayList<>();
+        adapter = new MessageAdapter(messages);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString().trim();
+                if (!text.isEmpty()) {
+                    Message message = new Message(text, "John Wayne");
+                    messages.add(message);
+                    adapter.notifyItemInserted(messages.size() - 1);
+                    recyclerView.scrollToPosition(messages.size() - 1);
+                    editText.getText().clear();
+                }
+            }
+        });
+
+        closeLiveScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), EndedLiveScreen.class));
+            }
+        });
     }
+
+    void showMessage(String message) {
+        runOnUiThread(() ->
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show());
+    }
+
+    private boolean checkSelfPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this, REQUESTED_PERMISSIONS[0]) !=  PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, REQUESTED_PERMISSIONS[1]) !=  PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void setupVideoSDKEngine() {
+        try {
+            RtcEngineConfig config = new RtcEngineConfig();
+            config.mContext = getBaseContext();
+            config.mAppId = appId;
+            config.mEventHandler = mRtcEventHandler;
+            agoraEngine = RtcEngine.create(config);
+            // By default, the video module is disabled, call enableVideo to enable it.
+            agoraEngine.enableVideo();
+        } catch (Exception e) {
+            showMessage(e.toString());
+        }
+    }
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the remote host joining the channel to get the uid of the host.
+        public void onUserJoined(int uid, int elapsed) {
+            showMessage("Remote user joined " + uid);
+
+            // Set the remote video view
+            runOnUiThread(() -> setupRemoteVideo(uid));
+        }
+
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            isJoined = true;
+            showMessage("Joined Channel " + channel);
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) {
+            showMessage("Remote user offline " + uid + " " + reason);
+            runOnUiThread(() -> remoteSurfaceView.setVisibility(View.GONE));
+        }
+    };
+
+    private void setupRemoteVideo(int uid) {
+        FrameLayout container = findViewById(R.id.remote_video_view_container);
+        remoteSurfaceView = new SurfaceView(getBaseContext());
+        remoteSurfaceView.setZOrderMediaOverlay(true);
+        container.addView(remoteSurfaceView);
+        agoraEngine.setupRemoteVideo(new VideoCanvas(remoteSurfaceView, VideoCanvas.RENDER_MODE_FIT, uid));
+        // Display RemoteSurfaceView.
+        remoteSurfaceView.setVisibility(View.VISIBLE);
+    }
+
+    private void setupLocalVideo() {
+        FrameLayout container = findViewById(R.id.local_video_view_container);
+        // Create a SurfaceView object and add it as a child to the FrameLayout.
+        localSurfaceView = new SurfaceView(getBaseContext());
+        container.addView(localSurfaceView);
+        // Call setupLocalVideo with a VideoCanvas having uid set to 0.
+        agoraEngine.setupLocalVideo(new VideoCanvas(localSurfaceView, VideoCanvas.RENDER_MODE_HIDDEN, 0));
+    }
+
+
+    public void joinChannel(View view) {
+        if (checkSelfPermission()) {
+            ChannelMediaOptions options = new ChannelMediaOptions();
+
+            // For a Video call, set the channel profile as COMMUNICATION.
+            options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION;
+            // Set the client role as BROADCASTER or AUDIENCE according to the scenario.
+            options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+            // Display LocalSurfaceView.
+            setupLocalVideo();
+            localSurfaceView.setVisibility(View.VISIBLE);
+            // Start local preview.
+            agoraEngine.startPreview();
+            // Join the channel with a temp token.
+            // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+            agoraEngine.joinChannel(token, channelName, uid, options);
+        } else {
+            Toast.makeText(getApplicationContext(), "Permissions was not granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void leaveChannel(View view) {
+        if (!isJoined) {
+            showMessage("Join a channel first");
+        } else {
+            agoraEngine.leaveChannel();
+            showMessage("You left the channel");
+            // Stop remote video rendering.
+            if (remoteSurfaceView != null) remoteSurfaceView.setVisibility(View.GONE);
+            // Stop local video rendering.
+            if (localSurfaceView != null) localSurfaceView.setVisibility(View.GONE);
+            isJoined = false;
+        }
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        agoraEngine.stopPreview();
+        agoraEngine.leaveChannel();
+
+        // Destroy the engine in a sub-thread to avoid congestion
+        new Thread(() -> {
+            RtcEngine.destroy();
+            agoraEngine = null;
+        }).start();
+    }
+
+
 }
--- app/src/main/java/com/example/infits/LiveSession.java
+++ app/src/main/java/com/example/infits/LiveSession.java
@@ -29,16 +29,5 @@ public class LiveSession extends AppCompatActivity {
         //customDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         CardView confirmCard = customDialog.findViewById(R.id.upcoming_stream_live);
         customDialog.show();
-
-        TextView setReminder = customDialog.findViewById(R.id.set_reminder);
-        GradientDrawable gradientDrawable = new GradientDrawable(
-                GradientDrawable.Orientation.TOP_BOTTOM,
-                new int[]{0xFF2667FF, 0xFFD553E7});
-
-// Set the corner radius of the GradientDrawable
-        gradientDrawable.setCornerRadius(50);
-
-// Set the GradientDrawable as the background of the TextView
-        setReminder.setBackground(gradientDrawable);
     }
 }
