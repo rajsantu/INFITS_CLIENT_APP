38ed084a3a25c63f26d6b2f9e6b805c027cf54fc
Prince <67627039+Prince488@users.noreply.github.com>
2023-07-16 01:34:54 +0530

    fixed ui spinner in diet chart

--- app/src/main/java/com/example/infits/fragment_diet_chart.java
+++ app/src/main/java/com/example/infits/fragment_diet_chart.java
@@ -9,19 +9,24 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.Spinner;
 
 import com.tenclouds.gaugeseekbar.GaugeSeekBar;
 
-public class fragment_diet_chart extends Fragment implements PopupMenu.OnMenuItemClickListener {
+public class fragment_diet_chart extends Fragment {
     GaugeSeekBar progressBar;
 
     ImageView back;
-   Button btn_meal_check,btn_meal;
+   Button btn_meal_check;
 
-   Button diet_choose_top_btn;
+
+
+   //for spinner
+    Spinner dailySpinner,btn_meal;
     public fragment_diet_chart () {
         // Required empty public constructor
     }
@@ -41,6 +46,18 @@ public class fragment_diet_chart extends Fragment implements PopupMenu.OnMenuIte
         View view = inflater.inflate ( R.layout.fragment_diet_chart , container , false );
         hooks(view);
 
+        //set daily weekly and yearly in spinner
+        ArrayAdapter<CharSequence> dailyAdapter = ArrayAdapter.createFromResource(requireActivity(),
+                R.array.spinner_diet_daily,R.layout.diet_spinner_dropdown_item );
+        dailyAdapter.setDropDownViewResource(R.layout.diet_spinner_layout);
+        dailySpinner.setAdapter(dailyAdapter);
+
+        //set breakfast snacks lunch Dinner
+        ArrayAdapter<CharSequence> breakfastAdapter = ArrayAdapter.createFromResource(requireActivity(),
+                R.array.spinner_diet_breakfast,R.layout.diet_spinner_dropdown_item );
+        breakfastAdapter.setDropDownViewResource(R.layout.diet_spinner_layout);
+        btn_meal.setAdapter(breakfastAdapter);
+
         back.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_fragment_diet_chart_to_dashBoardFragment));
 
         progressBar.setProgress(.75f);
@@ -48,53 +65,21 @@ public class fragment_diet_chart extends Fragment implements PopupMenu.OnMenuIte
 
         btn_meal_check.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_fragment_diet_chart_to_diet_fourth2));
 
-        btn_meal.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_fragment_diet_chart_to_diet_second_frag));
 
-        diet_choose_top_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //diet_choose_top_btn.setText();
-                showPopup(v);
-            }
-        });
+
 
         return view;
     }
 
-    public void showPopup(View v){
-        PopupMenu popup = new PopupMenu(getContext(),v);
-        popup.setOnMenuItemClickListener(this);
-        popup.inflate(R.menu.diet_popup_menu);
-        popup.show();
 
-    }
 
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item1:
-                diet_choose_top_btn.setText("Daily");
-                return true;
-            case R.id.item2:
-                diet_choose_top_btn.setText("Weekly");
-                return true;
-            case R.id.item3:
-                diet_choose_top_btn.setText("Monthly");
-                return true;
-            case R.id.item4:
-                diet_choose_top_btn.setText("Yearly");
-                return true;
-            default:
-                diet_choose_top_btn.setText("Choose");
-                return false;
-        }
-    }
 
     private void hooks(View view) {
         btn_meal_check=view.findViewById (R.id.button6);
         back=view.findViewById(R.id.imageView8);
         progressBar = view.findViewById(R.id.progressBar);
+        //for spinner
+        dailySpinner = view.findViewById(R.id.diet_btn_daily);
         btn_meal=view.findViewById(R.id.button8);
-        diet_choose_top_btn=view.findViewById(R.id.diet_btn_daily);
     }
 }
