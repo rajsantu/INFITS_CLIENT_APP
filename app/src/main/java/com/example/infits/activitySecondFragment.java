473b8b492163c1cc1d436a29063d4b6b76d0e66a
divyank <divyank.sharma2019@gmail.com>
2023-10-29 18:28:41 +0530

    Set_Goal_Activit_Tracker

--- app/src/main/java/com/example/infits/activitySecondFragment.java
+++ app/src/main/java/com/example/infits/activitySecondFragment.java
@@ -1,29 +1,36 @@
 package com.example.infits;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.tenclouds.gaugeseekbar.GaugeSeekBar;
 
-import org.joda.time.LocalDateTime;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.Date;
@@ -32,16 +39,17 @@ import java.util.Map;
 
 public class activitySecondFragment extends Fragment {
 
-    GaugeSeekBar progressBar;
+    //GaugeSeekBar progressBar;
     Button run_goal_btn,btn_start;
     TextView textView71, textView72, textView73;
     ImageView back_button;
+    ImageView set_goal;
+    EditText goal_value_txt;
+    String goal_value;
 
     public activitySecondFragment() {
         // Required empty public constructor
     }
-
-
     public static activitySecondFragment newInstance(String param1, String param2) {
         activitySecondFragment fragment = new activitySecondFragment();
         Bundle args = new Bundle();
@@ -68,12 +76,78 @@ public class activitySecondFragment extends Fragment {
         textView72 = view.findViewById(R.id.textView72);
         textView73 = view.findViewById(R.id.textView73);
         back_button=view.findViewById(R.id.imageView73);
-        run_goal_btn.setOnClickListener ( new View.OnClickListener () {
+        set_goal = dialog.findViewById(R.id.imageView89);
+        goal_value_txt = dialog.findViewById(R.id.textView88);
+
+        run_goal_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick ( View v ) {
-                dialog.show ();
+            public void onClick(View v) {
+                ImageView closeImageView = dialog.findViewById(R.id.imageView87);
+                dialog.show();
+                closeImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                set_goal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        goal_value = goal_value_txt.getText().toString();
+                        //Toast.makeText(getApplicationContext(), goal_value, Toast.LENGTH_SHORT).show();
+                        String goal = goal_value;
+                        String url = "http://10.12.2.128/infits/activitytracker.php";
+
+                        Log.d("Request", "Sending a request to: " + url);
+
+                        StringRequest request = new StringRequest(Request.Method.POST, url, response -> {
+                            Log.d("Response", "Received response: " + response);
+                            if (response.equals("Connectedupdated")) {
+                                Toast.makeText(getApplicationContext(),"Goal set : "+ goal_value, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                            }
+                            else if (response.equals("Connectedexists")) {
+                                Toast.makeText(getApplicationContext(), "Goal Already Set", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                System.out.println(response);
+                                Toast.makeText(getApplicationContext(), "Not working: " + String.valueOf(response), Toast.LENGTH_SHORT).show();
+                            }
+                        }, error -> {
+                            Log.e("RequestError", "Error: " + error.toString());
+                            Toast.makeText(getApplicationContext(), "Error: " + error.toString().trim(), Toast.LENGTH_SHORT).show();
+                        }) {
+                            @Nullable
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> data = new HashMap<>();
+                                data.put("client_id", DataFromDatabase.client_id);
+                                data.put("clientuserID", DataFromDatabase.clientuserID);
+                                data.put("distance", "0");
+                                data.put("calories", "0");
+                                data.put("runtime", "0");
+                                data.put("goal", goal);
+                                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                                java.time.LocalDateTime now = LocalDateTime.now();
+                                data.put("date", dtf.format(now));
+                                data.put("categories", "Running");
+                                data.put("heartrate", "0");
+                                return data;
+                            }
+                        };
+                        request.setRetryPolicy(new DefaultRetryPolicy(
+                                10000,  // 10 seconds
+                                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+                        ));
+                        Volley.newRequestQueue(getApplicationContext().getApplicationContext()).add(request);
+                        //  Toast.makeText(getApplicationContext(),"check 1",Toast.LENGTH_SHORT).show();
+
+                        dialog.dismiss();
+                    }
+                });
             }
-        } );
+        });
 
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/infits/activitythirdfragment.java
+++ app/src/main/java/com/example/infits/activitythirdfragment.java
@@ -1,8 +1,11 @@
 package com.example.infits;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 import android.app.Dialog;
 import android.os.Bundle;
-
+import android.util.Log;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -10,14 +13,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class activitythirdfragment extends Fragment {
 
-    Button btn_setgoal, btn_start_trd;
+    //Button btn_setgoal, btn_start_trd;
+    Button run_goal_btn,btn_start;
+    TextView textView71, textView72, textView73;
     ImageView back_button;
+    ImageView set_goal;
+    EditText goal_value_txt;
+    String goal_value;
 
     public activitythirdfragment() {
         // Required empty public constructor
@@ -49,17 +71,83 @@ public class activitythirdfragment extends Fragment {
 
         Dialog dialog=new Dialog (this.getContext ());
         dialog.setContentView ( R.layout.activity_trck_popup );
-        btn_start_trd=view.findViewById ( R.id.imageView86_trd);
-        btn_setgoal=view.findViewById ( R.id.imageView74 );
-        back_button=view.findViewById( R.id.imageView73);
-        btn_setgoal.setOnClickListener ( new View.OnClickListener () {
+        run_goal_btn = view.findViewById(R.id.imageView74);
+        btn_start = view.findViewById(R.id.imageView86_trd);
+        textView71 = view.findViewById(R.id.textView71);
+        textView72 = view.findViewById(R.id.textView72);
+        textView73 = view.findViewById(R.id.textView73);
+        back_button=view.findViewById(R.id.imageView73);
+        set_goal = dialog.findViewById(R.id.imageView89);
+        goal_value_txt = dialog.findViewById(R.id.textView88);
+        run_goal_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick ( View v ) {
-                dialog.show ();
-            }
-        } );
+            public void onClick(View v) {
+                ImageView closeImageView = dialog.findViewById(R.id.imageView87);
+                dialog.show();
+                closeImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss(); // Close the dialog
+                    }
+                });
+                set_goal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        goal_value = goal_value_txt.getText().toString();
+                        //Toast.makeText(getApplicationContext(), goal_value, Toast.LENGTH_SHORT).show();
+                        String goal = goal_value;
+                        String url = "http://10.12.2.128/infits/activitytracker.php";
 
-        btn_start_trd.setOnClickListener ( new View.OnClickListener () {
+                        Log.d("Request", "Sending a request to: " + url);
+
+                        StringRequest request = new StringRequest(Request.Method.POST, url, response -> {
+                            Log.d("Response", "Received response: " + response);
+                            if (response.equals("Connectedupdated")) {
+                                Toast.makeText(getApplicationContext(),"Goal set : "+ goal_value, Toast.LENGTH_SHORT).show();
+                              //  Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                            } else if (response.equals("Connectedexists")) {
+                                Toast.makeText(getApplicationContext(), "Goal Already Set", Toast.LENGTH_SHORT).show();
+                            } else {
+                                System.out.println(response);
+                                Toast.makeText(getApplicationContext(), "Not working: " + String.valueOf(response), Toast.LENGTH_SHORT).show();
+                            }
+                        }, error -> {
+                            Log.e("RequestError", "Error: " + error.toString());
+                            Toast.makeText(getApplicationContext(), "Error: " + error.toString().trim(), Toast.LENGTH_SHORT).show();
+                        }) {
+                            @Nullable
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> data = new HashMap<>();
+                                data.put("client_id", DataFromDatabase.client_id);
+                                data.put("clientuserID", DataFromDatabase.clientuserID);
+                                data.put("distance", "0");
+                                data.put("calories", "0");
+                                data.put("runtime", "0");
+                                data.put("goal", goal);
+                                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                                java.time.LocalDateTime now = LocalDateTime.now();
+                                data.put("date", dtf.format(now));
+                                data.put("categories", "Cycling");
+                                data.put("heartrate", "0");
+                                return data;
+                            }
+                        };
+                        request.setRetryPolicy(new DefaultRetryPolicy(
+                                10000,  // 10 seconds
+                                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+                        ));
+                        Volley.newRequestQueue(getApplicationContext().getApplicationContext()).add(request);
+                        //  Toast.makeText(getApplicationContext(),"check 1",Toast.LENGTH_SHORT).show();
+
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        btn_start.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick ( View v ) {
                 Navigation.findNavController(v).navigate(R.id.action_activitythirdfragment_to_cycling_frag2);
